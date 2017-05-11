2d8b6c1a9faef7c52cd05e640ad51433b8e25f96
laksonosuryadi <laksono.suryadi@gmail.com>
2017-05-11 13:51:05 +0700

    tes

--- PlanItNow/src/components/index.js
+++ PlanItNow/src/components/index.js
@@ -6,9 +6,9 @@ import inputQuery from './inputQuery';
 import Register from './Register';
 
 const App = StackNavigator({
-  Main: {screen: Login},
-  Register: {screen: Register},
-  inputQuery: {screen: inputQuery}
+  //Main: {screen: Login},
+  Register: {screen: Register}
+  //inputQuery: {screen: inputQuery}
 })
 
 export default App;
