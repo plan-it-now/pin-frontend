3c5165dc8a0e738a8474f83cef425335a8ab9236
daniel <aguspermadi94@gmail.com>
2017-05-12 20:30:38 +0700

    resolve nothing to changes

--- PlanItNow/src/components/Profile/index.js
+++ PlanItNow/src/components/Profile/index.js
@@ -5,6 +5,16 @@ import {
   Text
 } from 'react-native';
 
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button, Left, Right, Body, Icon
+} from 'native-base';
+
 class Profile extends React.Component {
   constructor(props) {
     super(props)
@@ -13,11 +23,30 @@ class Profile extends React.Component {
 
   render () {
     return (
-      <View>
-        <Text>
-          Users Profile
-        </Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparant>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>
+              Your Name
+            </Title>
+          </Body>
+        </Header>
+
+        <Content>
+          <Text>Test</Text>
+        </Content>
+
+        <Footer backgroundColor="#fff">
+          <Button block>
+            <Text>Add</Text>
+          </Button>
+        </Footer>
+      </Container>
     )
   }
 
