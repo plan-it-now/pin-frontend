391e8615192d67f8d6df4df6ea0abae8a7afc38e
daniel <aguspermadi94@gmail.com>
2017-05-12 20:15:14 +0700

    starting develop user profile

--- PlanItNow/src/components/index.js
+++ PlanItNow/src/components/index.js
@@ -8,14 +8,16 @@ import Register from './Register';
 import Recomendation from './Recomendation';
 import ItineraryStepOne from './ItineraryStepOne';
 import step2 from './step2';
+import Profile from './Profile';
 
 const App = StackNavigator({
+  Profile: {screen : Profile}
   // Login: {screen: Login},
   // Register: {screen: Register},
   // inputQuery: {screen: inputQuery},
   // Recomendation: {screen: Recomendation},
   // ItineraryStepOne: {screen: ItineraryStepOne}
-  step2: {screen: step2}
+  // step2: {screen: step2}
 }, {
   headerMode: 'none'
 })
