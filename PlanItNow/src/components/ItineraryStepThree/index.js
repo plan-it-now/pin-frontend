574f85dae8bdbc84d61c4fe880c44e5cb9acb028
daniel <aguspermadi94@gmail.com>
2017-05-12 13:44:09 +0700

    resolve confilct

---
+++ PlanItNow/src/components/ItineraryStepThree/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native'
+
+class ItineraryStepThree extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render () {
+    return (
+      <View>
+        <Text>Details 3</Text>
+      </View>
+    )
+
+  }
+
+}
+
+export default ItineraryStepThree;
