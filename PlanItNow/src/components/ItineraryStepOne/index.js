da944bb9751b245c4d4c3c848aa984d5c3abdc0c
zidane9 <d4rr@hotmail.com>
2017-05-13 21:30:18 +0700

    stepone

--- PlanItNow/src/components/ItineraryStepOne/index.js
+++ PlanItNow/src/components/ItineraryStepOne/index.js
@@ -25,20 +25,39 @@ class ItineraryStepOne extends React.Component {
     super(props)
     this.state = {
       selectedItem: undefined,
-      selected1: 'key1',
+      selectedDay: [],
       results: {
         items: []
       }
     }
   }
 
-  onValueChange ( value: String ) {
+  componentDidMount(){
+    let arraySelected = new Array(this.props.places.approvedPlaces.length);
+    for(let i=0;i<arraySelected.length;i++){
+      arraySelected[i] = 1;
+    }
     this.setState({
-      selected1: value
+      selectedDay: arraySelected
+    })
+  }
+
+  onValueChange (value: Number, index) {
+    // console.log('v',value);
+    // console.log('idx', index);
+    // console.log('s', this.state.selectedDay);
+    let arrTmp = this.state.selectedDay;
+    arrTmp[index] = value;
+    this.setState({
+      selectedDay: arrTmp
     })
   }
 
   render () {
+    const tmp = new Array(this.props.places.days);
+    for(let i=1;i<=tmp.length;i++){
+      tmp[i-1] = 'Day '+ i;
+    }
     return (
       <Container
         style={{ backgroundColor: '#B39DDB' }}
@@ -47,49 +66,55 @@ class ItineraryStepOne extends React.Component {
           <Content
             padder
           >
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                backgroundColor: 'white',
-                height: 80,
-                marginBottom: 10,
-                marginTop: 10
-              }}
-            >
-              <View
-                style={{
-                  width: 200,
-                  paddingLeft: 10
-                }}
-              >
-                <Text
+            {
+              this.props.places.approvedPlaces.map( (place,idx) => (
+                <View key={idx}
                   style={{
-                    color: '#000',
-                    fontSize: 18
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    backgroundColor: 'white',
+                    height: 80,
+                    marginBottom: 10,
+                    marginTop: 10
                   }}
-                >Pulau Dewata </Text>
-              </View>
-              <View style={{
-                  width: 120,
-                  paddingRight: 10
-                }}
-              >
-                <Picker
-                  supportedOrientations={['portrait','landscape']}
-                  iosHeader="Select one"
-                  mode="dropdown"
-                  selectedValue={this.state.selected1}
-                  onValueChange={this.onValueChange.bind(this)}>
-                    <Item label="Day 1" value="key0" />
-                    <Item label="Day 2" value="key1" />
-                    <Item label="Day 3" value="key2" />
-                    <Item label="Day 4" value="key3" />
-                </Picker>
-              </View>
-            </View>
+                >
+                  <View
+                    style={{
+                      width: 200,
+                      paddingLeft: 10
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: '#000',
+                        fontSize: 18
+                      }}
+                    >{place.name} </Text>
+                  </View>
+                  <View style={{
+                      width: 120,
+                      paddingRight: 10
+                    }}
+                  >
+                    <Picker
+                      supportedOrientations={['portrait','landscape']}
+                      iosHeader="Select one"
+                      mode="dropdown"
+                      selectedValue={this.state.selectedDay[idx]}
+                      onValueChange={(value)=>this.onValueChange(value,idx)}>
+                      {
+                        tmp.map((day,index)=>(
+                          <Item key={index} label={day} value={index+1} />
+                        ))
+                      }
+
+                    </Picker>
+                  </View>
+                </View>
+              ))
+            }
 
           </Content>
         </ScrollView>
