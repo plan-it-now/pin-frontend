538865e1863331fe66a1a3abc55e11b73a1df736
daniel <aguspermadi94@gmail.com>
2017-05-13 11:32:16 +0700

    resolve conflict again

---
+++ PlanItNow/src/components/Profile/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render () {
+    return (
+      <View>
+        <Text>
+          Users Profile
+        </Text>
+      </View>
+    )
+  }
+
+}
+
+export default Profile;
--- PlanItNow/src/components/index.js
+++ PlanItNow/src/components/index.js
@@ -9,6 +9,7 @@ import Recomendation from './Recomendation';
 import ItineraryStepOne from './ItineraryStepOne';
 import ItineraryStepThree from './ItineraryStepThree';
 import step2 from './step2';
+import Profile from './Profile';
 
 const App = StackNavigator({
   Login: {screen: Login},
