b44986fad1e8e32223ed2eae3cc6340e66bd02cb
daniel <aguspermadi94@gmail.com>
2017-05-13 13:12:55 +0700

    users profile half done

--- PlanItNow/src/components/Profile/index.js
+++ PlanItNow/src/components/Profile/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {
   View,
-  Text
+  Image
 } from 'react-native';
 
 import {
@@ -12,8 +12,17 @@ import {
   Content,
   Footer,
   FooterTab,
-  Button, Left, Right, Body, Icon
+  Button,
+  Left,
+  Right,
+  Body,
+  Card,
+  CardItem,
+  Text,
 } from 'native-base';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 class Profile extends React.Component {
   constructor(props) {
@@ -23,11 +32,15 @@ class Profile extends React.Component {
 
   render () {
     return (
-      <Container>
-        <Header>
+      <Container style={{ backgroundColor: '#fff' }}>
+        <Header style={{ backgroundColor: '#fc5757' }}>
           <Left>
-            <Button transparant>
-              <Icon name='menu' />
+            <Button style={{
+              backgroundColor: '#fc5757',
+
+            }}>
+              <Icon name="menu" color="white" size={22}
+              />
             </Button>
           </Left>
           <Body>
@@ -37,15 +50,57 @@ class Profile extends React.Component {
           </Body>
         </Header>
 
-        <Content>
-          <Text>Test</Text>
+        <Content padder>
+          <Card>
+            <View style={{  }}>
+              <Image
+                style={{ width: '100%', height: 200, backgroundColor: 'hsl(360, 100%, 100%)' }}
+                source={{ uri : 'http://alindstransport.com/wp-content/uploads/2016/09/Tugu-Jogja-Yogyakarta-jogja.jpg' }} />
+              <CardItem>
+                <Body>
+                  <Text>
+                    Yogyakarta, Indonesia
+                  </Text>
+                  <Text note>
+                    12 Mei 2017
+                  </Text>
+                </Body>
+              </CardItem>
+            </View>
+
+            <View>
+              <Image
+                style={{ width: '100%', height: 200, backgroundColor: '#696969' }}
+                source={{ uri : 'https://4.bp.blogspot.com/-4X3c0CA9Qp4/V-t1RvcGgII/AAAAAAAAAWY/CM_VOv8vjeYRpfnn6FJeIoZP44jQx6gSACLcB/s1600/600.PNG' }} />
+              <CardItem>
+                <Body>
+                  <Text>
+                    Yogyakarta, Indonesia
+                  </Text>
+                  <Text>
+                    12 Mei 2017
+                  </Text>
+                </Body>
+              </CardItem>
+            </View>
+          </Card>
         </Content>
 
-        <Footer backgroundColor="#fff">
-          <Button block>
-            <Text>Add</Text>
-          </Button>
-        </Footer>
+        <Button
+          style={{
+            width: 60,
+            height: 60,
+            backgroundColor: '#fc5757',
+            position: 'absolute',
+            bottom: 10,
+            right: 20,
+            borderRadius: 100
+          }}>
+          <Icon name="plus" color="white" size={26} style={{
+            textAlign: 'center',
+            }}/>
+        </Button>
+
       </Container>
     )
   }
--- PlanItNow/src/components/index.js
+++ PlanItNow/src/components/index.js
@@ -12,13 +12,14 @@ import step2 from './step2';
 import Profile from './Profile';
 
 const App = StackNavigator({
-  Login: {screen: Login},
-  Register: {screen: Register},
-  inputQuery: {screen: inputQuery},
-  Recomendation: {screen: Recomendation},
+  // Login: {screen: Login},
+  // Register: {screen: Register},
+  // inputQuery: {screen: inputQuery},
+  // Recomendation: {screen: Recomendation},
   // ItineraryStepOne: {screen: ItineraryStepOne},
   // step2: {screen: step2},
   // ItineraryStepThree: {screen: ItineraryStepThree},
+  Profile: {screen: Profile},
 }, {
   headerMode: 'none'
 })
