1d3ca52a7360d42e1df295110f25b8f0226b8c6c
zidane9 <d4rr@hotmail.com>
2017-05-15 18:25:58 +0700

    saving to db done

--- PlanItNow/src/actions/index.js
+++ PlanItNow/src/actions/index.js
@@ -1,4 +1,5 @@
 import { login, signup, updateUser } from './userAction';
 import { fetchPlaces, processPlaces, processStep1, processStep2 } from './placeAction';
+import { postItinerary } from './itineraryAction';
 
-export { login, signup, fetchPlaces, processPlaces, processStep1, processStep2,updateUser };
+export { login, signup, fetchPlaces, processPlaces, processStep1, processStep2,updateUser,postItinerary };
