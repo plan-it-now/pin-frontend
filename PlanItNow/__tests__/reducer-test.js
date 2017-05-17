315a269b89c9ca6739ce5adcd2d0c51777d089b6
daniel <aguspermadi94@gmail.com>
2017-05-17 19:11:30 +0700

    half done testing reducer

--- PlanItNow/__tests__/reducer-test.js
+++ PlanItNow/__tests__/reducer-test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import 'react-native';
+
 import userReducer from '../src/reducers/userReducer';
+import itineraryReducer from '../src/reducers/itineraryReducer';
+import placeReducer from '../src/reducers/placeReducer';
+
 import { loginSuccess, signUp } from '../src/actions/userAction';
 
 import {
   SIGNUP_USER,
-  LOGOUT_USER
+  LOGOUT_USER,
+  LOGIN_FB,
+  UPDATE_REDIRECT,
+  POST_ITIN,
+  FETCH_ITIN,
+  FETCH_PLACES
 } from '../src/actions/constants';
 
 const initialState = {
@@ -57,5 +66,52 @@ describe('User Reducers', () => {
     })
   })
 
+  it('handle LOGOUT action SUCCESS', () => {
+    const actions = {
+      type: UPDATE_REDIRECT,
+      payload: {payloadKey: 'payloadVal'}
+    }
+    const newState = userReducer(undefined, actions)
+    expect(newState).toEqual({
+      "shouldRedirectSignIn": false,
+      "shouldRedirectSignUp": false,
+      "token" : "",
+      "userdata": "",
+      "warning": ""
+    })
+  })
+})
 
+describe('Itinerary Reducer', () => {
+  it('handle FETCH ITINERARY USER action SUCCESS', () => {
+    const actions = {
+      type: FETCH_ITIN,
+      payload: {payloadKey: 'payloadVal'}
+    }
+    const newState = itineraryReducer(undefined, actions)
+    expect(newState).toEqual({
+      "payloadKey": "payloadVal"
+    })
+  })
+})
+
+describe('Places Reducer', () => {
+  it('handle Placer Reducer', () => {
+    const actions = {
+      type: FETCH_PLACES,
+      payload: {payload: 'payloadVal'}
+    }
+    const newState = placeReducer(undefined, actions)
+    expect(newState).toEqual({
+      days: 0,
+      recomendationPlaces: [{
+        description: "",
+        name: "Loading...",
+        photo: "",
+      }],
+      approvedPlaces: [],
+      payload: "payloadVal",
+      rejectedPlaces: []
+    })
+  })
 })
